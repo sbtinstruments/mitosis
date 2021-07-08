da66c2fef6b399d6598001cf98cadec3dbdbd791
Jonatan Midtgaard <jonatan.midtgaard@gmail.com>
2021-07-08 14:27:22 +0200

    Rework: FlowModel now main in-memory reperesentation

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-#from anyio import TASK_STATUS_IGNORED, create_task_group, run
-#from anyio.abc import TaskStatus
+from anyio import TASK_STATUS_IGNORED, create_task_group, run
+from anyio.abc import TaskStatus
 
 #from async_node import ProducerNode, AveragerNode
 
@@ -8,11 +8,13 @@ import networkx as nx
 import json
 
 import logging
+from networkx.algorithms.operators.unary import reverse
 
 from networkx.algorithms.shortest_paths.unweighted import predecessor
+from networkx.algorithms.dag import topological_sort
 import util
 
-from graph_model import GraphModel, NodeType, Edge
+from flow_model import FlowModel, FlowValidationException
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -35,69 +37,79 @@ _LOGGER = logging.getLogger(__name__)
 
 # run(main)
 
-def edge_comparator(a: Edge, b: Edge):
-    """This can be used to sort edges lexicographically by their nodes"""
 
-def validate_graph_model(G: GraphModel):
+
+def validate_flow_model(G: FlowModel):
     """Validates a Graph Model to our custom specs"""
     # Check if edges reference existing nodes
     for edge in G.edges:
-        if edge.start not in G.nodes:
+        if edge.start.node not in G.nodes:
             _LOGGER.error(f'The node {edge.start} does not exist')
             return False
-        if edge.end not in G.nodes:
+        if edge.end.node not in G.nodes:
             _LOGGER.error(f'The node {edge.end} does not exist')
             return False
     # Check that number of inputs match number of edges going in
     for node_name, node in G.nodes.items():
-        num_input_edges = sum(1 for edge in G.edges if edge.end == node_name)
-        num_input_types = 0 if node.input_types is None else len(node.input_types)
+        num_input_edges = sum(1 for edge in G.edges if edge.end.node == node_name)
+        num_input_types = 0 if node.inputs is None else len(node.inputs)
         if num_input_types != num_input_edges:
             _LOGGER.error(f'The node {node_name} does not have the correct number of inputs ({num_input_types} but it should have {num_input_edges})')
             return False
-    # For all inputs, check that the origin has the same type
-    for node_name, node in G.nodes.items():
-        if node.input_types is not None:
-            # Order input edges lexicographically
-            input_edges = sorted([e for e in G.edges if e.end == node_name], key=lambda e:e.start)
-            assert(len(input_edges) == len(node.input_types))
-            for input_type, input_edge in zip(node.input_types, input_edges):
-                if input_type != G.nodes[input_edge.start].output_type:
-                    predecessor_name = input_edge.start
-                    predecessor = G.nodes[predecessor_name]
-                    output_type = predecessor.output_type
-                    _LOGGER.error(f'Output type ({output_type}) in node {predecessor_name} does not match input type ({input_type}) in node {node_name}')
-                    return False
+
+    # For all edges, check that begin- and end-node has same type.
+    for edge in G.edges:
+        if G.nodes[edge.start.node].outputs[edge.start.port].datatype != G.nodes[edge.end.node].inputs[edge.end.port].datatype:
+            _LOGGER.error(f'The input and output type on the edge {edge} does not match')
+            return False
+    
+    # TODO: typecheck source/execute()-functions
+
     return True
 
 
-def create_graph(graph_file: Path):
-    """This creates a DiGraph object from a graph.json file"""
-    # Create model
-    model = GraphModel.parse_file(graph_file)
-    # Validate model
-    if not validate_graph_model(model):
-        _LOGGER.error(f'Graph model not valid')
-        return None
+def boot_order(model: FlowModel):
+    """
+    This returns a topological sort of nodes, which gives us the correct 
+    order to spin up nodes. An error in this sorting also detects cycles
+    """
     # Create DiGraph object
     G = nx.DiGraph()
     # Create edges
     edges= []
     for edge in model.edges:
-        edge_tuple = (edge.start, edge.end)
+        edge_tuple = (edge.start.node, edge.end.node)
         edges.append(edge_tuple)
     G.add_edges_from(edges)
-    # Add metadata to existing nodes
-    node_data = model.nodes
-    for node in G.nodes:
-        G.add_node(node, data=node_data[node])
-    return G
+    try:
+        topo_sorted = [a for a in topological_sort(G)]
+    except nx.NetworkXUnfeasible as exc:
+        _LOGGER.error(f'The flow graph contains cycles')
+        raise FlowValidationException from exc
+    return reversed([a for a in topological_sort(G)])
 
 
-G = create_graph(Path('mygraph.json'))
-print(G.nodes)
-# G = create_graph(Path('mygraph.json'))
-# graph_valid = validate_graph(G)
+def create_flow_model(graph_file: Path):
+    """This creates a FlowModel from a json file"""
+    # Create model
+    model = FlowModel.parse_file(graph_file)
+    # Validate model
+    if not validate_flow_model(model):
+        _LOGGER.error(f'Flow model not valid')
+        return None
+    return model
 
-# print(list(G.predecessors('D')))
-# print(G.nodes['A'])
+
+# spawn tasks
+async def main():
+    # Create Flow Model
+    G = FlowModel.parse_file(Path('mygraph.json'))
+    boot_order = G.boot_order()
+    # Spawn Tasks
+    async with create_task_group() as task_group:
+        for node_name in boot_order:
+            node = AsyncNode(G.nodes[node_name])
+            averager_node = await task_group.start(node, 0.5)
+            await task_group.start(start_producer_node, 3, averager_node)
+
+run(main)
---
+++ async_node/asyncnode.py
@@ -0,0 +1,7 @@
+
+
+class AsyncNode:
+    
+    def __init__(self):
+        self.ins: InGroup = None
+        self.outs: OutGroup = None
